0e2dfe7a9ba0cdd1f9de2732b6502003cbddbb03
Lucio Baglione <coire1@gmail.com>
2022-11-10 12:16:55 +0100

    Fix login window.

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,36 +1,38 @@
 <template>
-  <div class="content">
-    <div class="columns is-multiline">
-      <div class="column is-8">
-        <h1 class="is-size-1">Login</h1>
-        <p>Login as Funded Proposer, Challenge Team member or IOG member to interact with milestones.</p>
-      </div>
-      <div class="column is-6">
-        <o-field label="Email">
-          <o-input v-model="email" type="email"></o-input>
-        </o-field>
-        <o-field label="Password">
-          <o-input v-model="password" type="password"></o-input>
-        </o-field>
-        <div class="buttons mt-6">
-          <o-button
-            variant="primary"
-            size="medium"
-            @click="handleLogin"
-            type="submit">
-              {{ loginMsg }}
-          </o-button>
-          <o-button
-            @click="handleReset"
-            variant="primary"
-            size="medium"
-            :disabled="loading || email.length === 0">
-            Reset Password
-          </o-button>
+  <section class="section">
+    <div class="content">
+      <div class="columns is-multiline">
+        <div class="column is-8">
+          <h1 class="is-size-1">Login</h1>
+          <p>Login as Funded Proposer, Challenge Team member or IOG member to interact with milestones.</p>
+        </div>
+        <div class="column is-6">
+          <o-field label="Email">
+            <o-input v-model="email" type="email"></o-input>
+          </o-field>
+          <o-field label="Password">
+            <o-input v-model="password" type="password"></o-input>
+          </o-field>
+          <div class="buttons mt-6">
+            <o-button
+              variant="primary"
+              size="medium"
+              @click="handleLogin"
+              type="submit">
+                {{ loginMsg }}
+            </o-button>
+            <o-button
+              @click="handleReset"
+              variant="primary"
+              size="medium"
+              :disabled="loading || email.length === 0">
+              Reset Password
+            </o-button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script setup>
