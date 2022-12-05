a840dc67b6054bb60172de6b250ba7a589a5be5a
Lucio Baglione <coire1@gmail.com>
2022-12-05 11:38:03 +0100

    Include all the SoM states in the data seed.

--- milestone-tracker-backend/volumes/db/init/data.sql
+++ milestone-tracker-backend/volumes/db/init/data.sql
@@ -1478,3 +1478,38 @@ INSERT INTO public.proposals_users (proposal_id, user_id, user_idd)
   VALUES
     (1, (SELECT user_id FROM public.users WHERE email = 'proposer-1@example.org' LIMIT 1), (SELECT id FROM public.users WHERE email = 'proposer-1@example.org' LIMIT 1)),
     (2, (SELECT user_id FROM public.users WHERE email = 'proposer-2@example.org' LIMIT 1), (SELECT id FROM public.users WHERE email = 'proposer-2@example.org' LIMIT 1));
+
+INSERT INTO public.soms (outputs, success_criteria, evidence, month, cost, completion, proposal_id, milestone, title, challenge_id, user_id)
+  VALUES
+    ('Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet', '3', 10000, 30, 1, 1, 'Title ipsum', 1, (SELECT user_id FROM public.users WHERE email = 'proposer-1@example.org' LIMIT 1)),
+    ('Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet', '3', 10000, 30, 1, 1, 'Title ipsum 2 try', 1, (SELECT user_id FROM public.users WHERE email = 'proposer-1@example.org' LIMIT 1)),
+    ('Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet', '3', 10000, 30, 1, 2, 'Title ipsum 2', 1, (SELECT user_id FROM public.users WHERE email = 'proposer-1@example.org' LIMIT 1)),
+    ('Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet', '3', 10000, 30, 1, 3, 'Title ipsum 3', 1, (SELECT user_id FROM public.users WHERE email = 'proposer-1@example.org' LIMIT 1)),
+    ('Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet', '3', 10000, 30, 1, 4, 'Title ipsum 4', 1, (SELECT user_id FROM public.users WHERE email = 'proposer-1@example.org' LIMIT 1)),
+    ('Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet', '3', 10000, 30, 1, 5, 'Title ipsum 5', 1, (SELECT user_id FROM public.users WHERE email = 'proposer-1@example.org' LIMIT 1));
+
+INSERT INTO public.som_reviews (outputs_approves, outputs_comment, success_criteria_approves, success_criteria_comment, evidence_approves, evidence_comment, som_id, challenge_id, user_id)
+  VALUES
+    (true, 'Lorem ipsum dolor sit amet', true, 'Lorem ipsum dolor sit amet', true, 'Lorem ipsum dolor sit amet', 3, 1, (SELECT user_id FROM public.users WHERE email = 'challenge-team-1@example.org' LIMIT 1)),
+    (true, 'Lorem ipsum dolor sit amet', true, 'Lorem ipsum dolor sit amet', true, 'Lorem ipsum dolor sit amet', 4, 1, (SELECT user_id FROM public.users WHERE email = 'challenge-team-1@example.org' LIMIT 1)),
+    (true, 'Lorem ipsum dolor sit amet', true, 'Lorem ipsum dolor sit amet', true, 'Lorem ipsum dolor sit amet', 5, 1, (SELECT user_id FROM public.users WHERE email = 'challenge-team-1@example.org' LIMIT 1)),
+    (true, 'Lorem ipsum dolor sit amet', true, 'Lorem ipsum dolor sit amet', true, 'Lorem ipsum dolor sit amet', 6, 1, (SELECT user_id FROM public.users WHERE email = 'challenge-team-1@example.org' LIMIT 1));
+
+INSERT INTO public.signoffs (som_id, user_id)
+  VALUES
+    (4, (SELECT user_id FROM public.users WHERE email = 'signoff@example.org' LIMIT 1)),
+    (5, (SELECT user_id FROM public.users WHERE email = 'signoff@example.org' LIMIT 1)),
+    (6, (SELECT user_id FROM public.users WHERE email = 'signoff@example.org' LIMIT 1));
+
+INSERT INTO public.poas (content, proposal_id, som_id, challenge_id, user_id)
+  VALUES
+    ('Lorem ipsum dolor sit amet', 1, 5, 1, (SELECT user_id FROM public.users WHERE email = 'proposer-1@example.org' LIMIT 1)),
+    ('Lorem ipsum dolor sit amet', 1, 6, 1, (SELECT user_id FROM public.users WHERE email = 'proposer-1@example.org' LIMIT 1));
+
+INSERT INTO public.poas_reviews (content_approved, content_comment, poas_id, user_id)
+  VALUES
+    (true, 'Lorem ipsum dolor sit amet', 2, (SELECT user_id FROM public.users WHERE email = 'iog@example.org' LIMIT 1));
+
+INSERT INTO public.signoffs (poa_id, user_id)
+  VALUES
+    (2, (SELECT user_id FROM public.users WHERE email = 'signoff@example.org' LIMIT 1));
