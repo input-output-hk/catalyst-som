ffde3d306bc13c12bac738eaa9cb7def9df8ffed
Lucio Baglione <coire1@gmail.com>
2023-01-09 13:17:31 +0100

    Login submitting form.

--- milestone-tracker-frontend/src/pages/Login.vue
+++ milestone-tracker-frontend/src/pages/Login.vue
@@ -7,28 +7,30 @@
           <p>Login as Funded Proposer, Challenge Team member or IOG member to interact with milestones.</p>
         </div>
         <div class="column is-6">
-          <o-field label="Email">
-            <o-input v-model="email" type="email"></o-input>
-          </o-field>
-          <o-field label="Password">
-            <o-input v-model="password" type="password"></o-input>
-          </o-field>
-          <div class="buttons mt-6">
-            <o-button
-              variant="primary"
-              size="medium"
-              @click="handleLogin"
-              type="submit">
-                {{ loginMsg }}
-            </o-button>
-            <o-button
-              @click="handleReset"
-              variant="primary"
-              size="medium"
-              :disabled="loading || email.length === 0">
-              Reset Password
-            </o-button>
-          </div>
+          <form @submit.prevent="handleLogin">
+            <o-field label="Email">
+              <o-input v-model="email" type="email"></o-input>
+            </o-field>
+            <o-field label="Password">
+              <o-input v-model="password" type="password"></o-input>
+            </o-field>
+            <div class="buttons mt-6">
+              <o-button
+                :disabled="password.length === 0 || email.length === 0"
+                variant="primary"
+                size="medium"
+                native-type="submit">
+                  {{ loginMsg }}
+              </o-button>
+              <o-button
+                @click="handleReset"
+                variant="primary"
+                size="medium"
+                :disabled="loading || email.length === 0">
+                Reset Password
+              </o-button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
