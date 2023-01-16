814b7cc33b7acc1df5894fa89ad3f972a0f12cdb
Lucio Baglione <coire1@gmail.com>
2023-01-16 16:26:06 +0100

    Add validation and schema-form mechanism to PoA review form.

--- milestone-tracker-frontend/src/components/forms/NewPoaReview.vue
+++ milestone-tracker-frontend/src/components/forms/NewPoaReview.vue
@@ -2,62 +2,69 @@
   <div class="content">
     <div class="box">
       <h3>New Review for Proof of Achievement</h3>
-      <div class="block">
-        <o-checkbox v-model="form.content_approved">
-          PoA Approved?
-        </o-checkbox>
-      </div>
-      <div class="block">
-        <label>PoA review comments:</label>
-        <QuillEditor
-          ref="contentEditor"
-          class="mb-4"
-          theme="snow" v-model:content="form.content_comment" content-type="html" />
-      </div>
-      <div class="block">
-        <o-button
-          variant="primary"
-          size="medium"
-          @click="handleCreatePoaReview"
-          type="submit">
-            Submit PoA review
-        </o-button>
-      </div>
+      <schema-form
+        class="card-content scrollable-modal"
+        :schema="schema"
+        @submit="handleCreatePoaReview"
+        >
+        <template #afterForm>
+          <div class="buttons">
+            <o-button variant="primary" native-type="submit">
+              <span>Submit PoA review</span>
+            </o-button>
+            <o-button @click="clearForm">
+              <span>Reset</span>
+            </o-button>
+          </div>
+        </template>
+      </schema-form>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue'
+import { ref, computed } from 'vue'
 const props = defineProps(['poa', 'som'])
 const emit = defineEmits(['poaReviewSubmitted'])
-import { useForm } from '@/composables/useForm.js'
+import { useFormFields } from '@/composables/useFormFields.js'
 import { usePoaReviews } from '@/store/poaReviews.js'
+import VeeValidatePlugin from "@formvuelate/plugin-vee-validate"
+import { SchemaFormFactory, useSchemaForm } from "formvuelate"
 const { createPoaReview } = usePoaReviews()
 
-const contentEditor = ref()
+const initialSchema = computed(() => {
+  return {
+    content_approved: {
+      type: 'checkbox',
+      label: `PoA Approved?`
+    },
+    content_comment: {
+      type: 'html',
+      label: 'Comment'
+    }
+  }
+})
 
-const initialForm = {
-  content_approved: false,
-  content_comment: ''
-}
-
-const { form, _clearForm } = useForm(initialForm)
+const { schema, clearForm } = useFormFields(initialSchema.value)
+const formData = ref({})
+const { updateFormModel } = useSchemaForm(formData)
+let SchemaForm = SchemaFormFactory([
+  VeeValidatePlugin(),
+])
 
 const handleCreatePoaReview = async () => {
   const response =  await createPoaReview({
-    ...form,
+    ...formData.value,
     poas_id: props.poa.id
   }, props.som)
   if (response) {
-    clearForm()
+    _clearForm()
     emit('poaReviewSubmitted')
   }
 }
 
-const clearForm = () => {
-  _clearForm()
-  contentEditor.value.setHTML('')
+const _clearForm = () => {
+  clearForm(formData, updateFormModel)
 }
 
 </script>
