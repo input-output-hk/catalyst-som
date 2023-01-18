fc98a1f9947d6b2c355a14aafa6b9acd9ff41b9c
Lucio Baglione <coire1@gmail.com>
2023-01-18 12:01:08 +0100

    Update i18n for nav.

--- milestone-tracker-frontend/src/components/NavBar.vue
+++ milestone-tracker-frontend/src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar is-primary has-text-weight-semibold" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <figure class="image p-3">
-        <img class="logo" :src="logoUrl" alt="Project Catalyst Milestone Module" />
+        <img class="logo" :src="logoUrl" :alt="$t('global.title')" />
       </figure>
       <a role="button"
         class="navbar-burger"
@@ -17,17 +17,17 @@
     </div>
     <div id="main-nav" class="navbar-menu" :class="{'is-active': navActive}">
       <div class="navbar-start">
-        <router-link class="navbar-item" to="/">Home</router-link>
-        <router-link class="navbar-item" to="/proposals">Proposals</router-link>
-        <router-link class="navbar-item" to="/login" v-if="!logged">Login</router-link>
-        <router-link class="navbar-item" to="/profile" v-if="logged">Profile</router-link>
-        <router-link class="navbar-item" to="/admin" v-if="isAdmin">Admin</router-link>
+        <router-link class="navbar-item" to="/">{{ $t('nav.home') }}</router-link>
+        <router-link class="navbar-item" to="/proposals">{{ $t('nav.proposals') }}</router-link>
+        <router-link class="navbar-item" to="/login" v-if="!logged">{{ $t('nav.login') }}</router-link>
+        <router-link class="navbar-item" to="/profile" v-if="logged">{{ $t('nav.profile') }}</router-link>
+        <router-link class="navbar-item" to="/admin" v-if="isAdmin">{{ $t('nav.admin') }}</router-link>
       </div>
       <div class="navbar-end" v-if="logged">
         <div class="navbar-item">
           <div class="buttons">
             <a class="button is-white">
-              <span class="logout" @click="logout">Logout {{user.email}}</span>
+              <span class="logout" @click="logout">{{ $t('nav.logout', {name: user.email}) }}</span>
             </a>
           </div>
         </div>
--- milestone-tracker-frontend/src/locales/messages/en.js
+++ milestone-tracker-frontend/src/locales/messages/en.js
@@ -1,4 +1,15 @@
 export default {
+  global: {
+    title: "Project Catalyst Milestone Module"
+  },
+  nav: {
+    home: "Home",
+    proposals: "Proposals",
+    login: "Login",
+    profile: "Profile",
+    admin: "Admin",
+    logout: "Logout {name}"
+  },
   milestone: {
     som: "Statement of Milestone {nr}",
     latest_som: "Latest submission from proposers of the Statement of Milestone {nr}.",
