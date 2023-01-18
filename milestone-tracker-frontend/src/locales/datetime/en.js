10c621fc6c3eadf28437d85566dc72e29eaf2822
Lucio Baglione <coire1@gmail.com>
2023-01-18 09:02:08 +0100

    Reorganize i18n files structure.

---
+++ milestone-tracker-frontend/src/locales/datetime/en.js
@@ -0,0 +1,14 @@
+export default {
+  short: {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  },
+  long: {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+  }
+}
--- milestone-tracker-frontend/src/setup/i18n.js
+++ milestone-tracker-frontend/src/setup/i18n.js
@@ -1,33 +1,16 @@
 import { createI18n } from 'vue-i18n'
 
+import messages from '@/locales/messages/index.js'
+import numberFormats from '@/locales/numbers/index.js'
+import datetimeFormats from '@/locales/datetime/index.js'
+
 export default function setupI18n(app) {
   const i18n = createI18n({
     locale: 'en',
     allowComposition: true,
-    numberFormats: {
-      "en": {
-        currency: {
-          style: "currency",
-          currency: "USD",
-        },
-      },
-    },
-    datetimeFormats: {
-      "en": {
-        short: {
-          year: 'numeric',
-          month: 'short',
-          day: 'numeric'
-        },
-        long: {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-          hour: 'numeric',
-          minute: 'numeric'
-        }
-      }
-    }
+    messages,
+    numberFormats,
+    datetimeFormats
   })
   app.use(i18n)
 }
