cbbb50cd89ca0ee50a1e1fab4f002f711d833b66
Lucio Baglione <coire1@gmail.com>
2023-01-26 14:11:15 +0100

    Add basic config for eslint and prettier.

--- milestone-tracker-frontend/vite.config.js
+++ milestone-tracker-frontend/vite.config.js
@@ -2,10 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslint({
+      extensions: ['.js', '.vue'],
+      ignorePath: '../.gitignore'
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
