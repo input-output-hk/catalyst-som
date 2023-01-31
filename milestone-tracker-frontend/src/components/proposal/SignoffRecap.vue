808bdeccfbf23d09ec861f0742541b721b352fcc
Lucio Baglione <coire1@gmail.com>
2023-01-31 19:54:03 +0100

    Lint SignoffRecap.

--- milestone-tracker-frontend/src/components/proposal/SignoffRecap.vue
+++ milestone-tracker-frontend/src/components/proposal/SignoffRecap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tile is-ancestor mb-0" v-if="signoffs.length > 0">
+  <div v-if="signoffs.length > 0" class="tile is-ancestor mb-0">
     <div class="tile is-12 is-parent">
       <div class="tile is-child notification is-success">
         <strong>{{ $t('signoff.poa_signed_off_at') }}</strong> {{ $d(signoffs[0].created_at, 'long') }}
