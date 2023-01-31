e54d50ccbc5f0fc8272c5199fe40a68a51bb4d46
Lucio Baglione <coire1@gmail.com>
2023-01-31 19:51:29 +0100

    Adjust vite config file for Docker env.

--- milestone-tracker-frontend/vite.config.js
+++ milestone-tracker-frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     eslint({
       extensions: ['.js', '.vue'],
-      ignorePath: '../.gitignore'
+      //ignorePath: '../.gitignore'
     })
   ],
   resolve: {
