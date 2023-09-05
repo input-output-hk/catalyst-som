5ffdacf13ae0c664de2d88971182d3904b8f4fac
Lucio Baglione <coire1@gmail.com>
2023-09-05 12:24:18 +0200

    fix: Expose Change requests URLs.

--- milestone-tracker-frontend/src/store/proposals.js
+++ milestone-tracker-frontend/src/store/proposals.js
@@ -63,7 +63,7 @@ export const useProposals = defineStore('proposals-store', {
       try {
         const { data, error } = await supabase
           .from('proposals')
-          .select('*, challenges(*), allocations(*, users(id, user_id, email)), change_request(created_at)')
+          .select('*, challenges(*), allocations(*, users(id, user_id, email)), change_request(id, created_at, url)')
           .eq('project_id', id)
         if (error) throw(error)
         return (data.length > 0) ? data[0] : {}
